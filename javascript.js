5e816c30a628ccbb30778bcdb2cb054a304e76ce
Aleksi012 <aleksi.anttila001@gmail.com>
2021-12-12 22:34:27 +0200

    o

--- javascript.js
+++ javascript.js
@@ -1,8 +1,50 @@
+let omistajat = document.getElementById("omistajat")
+
+omistajat.addEventListener("click", myFunction1)
+
+function myFunction1() {
+    document.getElementById("col1").innerHTML = ""
+    document.getElementById("col2").innerHTML = ""
+
+    document.getElementById("col1").innerHTML = "terve"
+    document.getElementById("col2").innerHTML = "terve2"
+}
+
+let premierleague = document.getElementById("premierleague")
+
+premierleague.addEventListener("click", myFunction2)
+
+function myFunction2() {
+    document.getElementById("col1").innerHTML = ""
+    document.getElementById("col2").innerHTML = ""
+
+    document.getElementById("col1").innerHTML = "terve"
+    document.getElementById("col2").innerHTML = "terve2"
+}
+
+
+let tärkeimmätpelaajat = document.getElementById("tärkeimmätpelaajat")
+
+tärkeimmätpelaajat.addEventListener("click", myFunction3)
+
+function myFunction3() {
+    document.getElementById("col1").innerHTML = ""
+    document.getElementById("col2").innerHTML = ""
+
+    document.getElementById("col1").innerHTML = "terve"
+    document.getElementById("col2").innerHTML = "terve2"
+}
+
+
+
 let pelipaidat = document.getElementById("pelipaidat")
-let image = document.getElementById("")
 
-pelipaidat.addEventListener("click", myFunction)
+pelipaidat.addEventListener("click", myFunction4)
 
-function myFunction() {
-    document.getElementById("col1").innerHTML
+function myFunction4() {
+    document.getElementById("col1").innerHTML = ""
+    document.getElementById("col2").innerHTML = ""
+
+    document.getElementById("col1").innerHTML = "terve"
+    document.getElementById("col2").innerHTML = "terve2"
 }
