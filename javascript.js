cb59cb6ed4f98ae0a47a024d16c6590f4d5a3794
Aleksi012 <aleksi.anttila001@gmail.com>
2021-12-12 21:48:27 +0200

    d

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let pelipaidat = document.getElementById("pelipaidat")
+let image = document.getElementById("")
+
+pelipaidat.addEventListener("click", myFunction)
+
+function myFunction() {
+    document.getElementById("col1").innerHTML
+}
