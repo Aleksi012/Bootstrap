2c44c7825b537fc48f5da56477ce1dc6d4699d44
Aleksi012 <aleksi.anttila001@gmail.com>
2021-12-15 19:29:37 +0200

    ö

--- javascript.js
+++ javascript.js
@@ -10,10 +10,9 @@ function myFunction1() {
     document.getElementById("col1").innerHTML = ""
     document.getElementById("col2").innerHTML = ""
 
-    let img = document.createElement("img");
-    img.src = "http://127.0.0.1:5500/images/radrizzani.jpg"; 
-    let src = document.getElementById("col2");
-    src.appendChild(img);
+    let x = document.createElement("img");
+    x.setAttribute("src", "images/radrizzani.jpg")
+    document.getElementById("col2").appendChild(x)
 
     document.getElementById("col1").innerHTML = "aoijswdaoiwdjaiowjdajwiodawiod oaiwdjaoiwdjoaiwjdaoiwdjoaiwdaiwjd"
 }
@@ -26,16 +25,9 @@ function myFunction2() {
     document.getElementById("col1").innerHTML = ""
     document.getElementById("col2").innerHTML = ""
 
-    let img = document.createElement("img");
-    img.src = ""; 
-    let src = document.getElementById("col1");
-    src.appendChild(img);
-
-    let img2 = document.createElement("img");
-    img2.src = "http://127.0.0.1:5500/images/premierleague.JPG";
-    let src2 = document.getElementById("col2")
-    src2.appendChild(img2)
-
+    let y = document.createElement("img");
+    y.setAttribute("src", "images/premierleague.JPG")
+    document.getElementById("col2").appendChild(y)
 }
 
 
@@ -49,37 +41,15 @@ function myFunction3() {
 
     document.getElementById("col1").innerHTML = "terveawdawdawdawdawdwadawdawdawd"
 
-    let img2 = document.createElement("img");
-    img2.src = "http://127.0.0.1:5500/images/pelaajat.JPG";
-    let src2 = document.getElementById("col2")
-    src2.appendChild(img2)
+    let x = document.createElement("img");
+    x.setAttribute("src", "images/pelaajat.JPG")
+    document.getElementById("col2").appendChild(x)
 
 }
-
-
 
 let pelipaidat = document.getElementById("pelipaidat")
 
 pelipaidat.addEventListener("click", myFunction4)
-
-
-/* 
-    function myFunction4() {
-    document.getElementById("col1").innerHTML = "terve"
-    document.getElementById("col2").innerHTML = "terve"
-
-    let img = document.createElement("img");
-    img.src = "http://127.0.0.1:5500/images/homekit2.JPG"; 
-    let src = document.getElementById("col1");
-    src.appendChild(img);
-
-    let img2 = document.createElement("img");
-    img2.src = "http://127.0.0.1:5500/images/awaykit2.JPG";
-    let src2 = document.getElementById("col2")
-    src2.appendChild(img2)
-
-}
-*/
 
 function myFunction4() {
 
